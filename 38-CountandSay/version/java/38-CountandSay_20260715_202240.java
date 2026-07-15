128c6fbd37fdc679357d64e55564de1fccb652c3
Rajasheka V <24243077@drngpit.ac.in>
2026-07-15 20:22:41 +0530

    Create 38-CountandSay

---
+++ 38-CountandSay/version/java/38-CountandSay_20260715_202240.java
@@ -0,0 +1,20 @@
+// Last updated: 7/15/2026, 8:22:40 PM
+1class Solution {
+2    public int[] plusOne(int[] digits) {
+3
+4        for (int i = digits.length - 1; i >= 0; i--) {
+5
+6            if (digits[i] < 9) {
+7                digits[i]++;
+8                return digits;
+9            }
+10
+11            digits[i] = 0;
+12        }
+13
+14        int[] result = new int[digits.length + 1];
+15        result[0] = 1;
+16
+17        return result;
+18    }
+19}
